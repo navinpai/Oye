4d76339d2fdc8e92aaab58e7c4e0d320c1fcf953
navinpai <lifeofnavin@gmail.com>
2019-03-25 00:14:00 +0530

    Update to have NotificationService working e2e

--- android/app/src/main/java/com/app/oye/MainActivity.java
+++ android/app/src/main/java/com/app/oye/MainActivity.java
@@ -1,52 +1,77 @@
 package com.app.oye;
 
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v4.app.NotificationCompat;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private TextView txtView;
+    private NotificationReceiver nReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent=new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
+        startActivity(intent);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        txtView = (TextView) findViewById(R.id.textView);
+        nReceiver = new NotificationReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.app.oye.NOTIFICATION_LISTENER_EXAMPLE");
+        registerReceiver(nReceiver,filter);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(nReceiver);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+    public void buttonClicked(View v){
+
+        if(v.getId() == R.id.btnCreateNotify){
+            NotificationManager nManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            NotificationCompat.Builder ncomp = new NotificationCompat.Builder(this);
+            ncomp.setContentTitle("My Notification");
+            ncomp.setContentText("Notification Listener Service Example");
+            ncomp.setTicker("Notification Listener Service Example");
+            ncomp.setSmallIcon(R.drawable.ic_launcher);
+            ncomp.setAutoCancel(true);
+            nManager.notify((int)System.currentTimeMillis(),ncomp.build());
+        }
+        else if(v.getId() == R.id.btnClearNotify){
+            Intent i = new Intent("com.app.oye.NOTIFICATION_LISTENER_SERVICE_EXAMPLE");
+            i.putExtra("command","clearall");
+            sendBroadcast(i);
+        }
+        else if(v.getId() == R.id.btnListNotify){
+            Intent i = new Intent("com.app.oye.NOTIFICATION_LISTENER_SERVICE_EXAMPLE");
+            i.putExtra("command","list");
+            sendBroadcast(i);
         }
 
-        return super.onOptionsItemSelected(item);
+
     }
+
+    class NotificationReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String temp = intent.getStringExtra("notification_event") + "\n" + txtView.getText();
+            txtView.setText(temp);
+        }
+    }
+
+
+
 }
